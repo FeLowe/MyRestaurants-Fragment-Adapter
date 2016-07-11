616efdf35ae9062905d0bc1b0ce854bbd2b0510b
Farnoosh Johnson and Fernanda Lowe <farnoosh+fernanda@>
2016-07-11 13:15:17 -0700

    add a valueEventListener and onDestroy method so eventListener stops running to save phone memory

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,8 +14,11 @@ import android.widget.TextView;
 import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 //import com.firebase.client.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //    private SharedPreferences.Editor mEditor;
 
     private DatabaseReference mSearchedLocationReference;
-
+    private ValueEventListener mSearchedLocationReferenceListener;
 
     @Bind(R.id.findRestaurantButton)
     Button mFindRestaurantsButton;
@@ -37,14 +41,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 //
-        mSearchedLocationReference = FirebaseDatabase.getInstance().getReference()
+        mSearchedLocationReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
                 .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
+
+        mSearchedLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
+                    String location = locationSnapshot.getValue().toString();
+                    Log.d("Locations updated", "location: " + location);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+
 
 //        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 //        mEditor = mSharedPreferences.edit();
@@ -67,6 +91,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             intent.putExtra("location", location);
             startActivity(intent);
         }
+
     }
 
     //    private void addToSharedPreferences(String location) {
@@ -75,5 +100,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void saveLocationToFirebase(String location) {
 //        Firebase searchedLocationRef = new Firebase(Constants.FIREBASE_URL_SEARCHED_LOCATION);
         mSearchedLocationReference.push().setValue(location);
+    }
+
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);
     }
 }
